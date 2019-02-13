6189b4dbfcb2a3ae72460d1ea6a73782cfd70def
Fabian Tamp <fabian.tamp@gmail.com>
2019-02-13 23:32:58 +0100

    A very very simple render for Xtrans sensors.

--- blitz/src/main.rs
+++ blitz/src/main.rs
@@ -6,6 +6,8 @@ fn main() {
     let file = libraw::RawFile::new("/Users/fabian/Pictures/2018/2018-12-02/ROFL6243.RAF".to_string()).unwrap();
     println!("Opened file: {:?}", file);
     dump_to_file("/tmp/thumb.jpg", file.get_jpeg_thumbnail()).unwrap();
+    let img = file.render_raw_preview();
+    img.save("/tmp/render.jpg").unwrap();
 }
 
 fn dump_to_file(filename: &str, data: &[u8]) -> std::io::Result<()> {
--- libraw/src/lib.rs
+++ libraw/src/lib.rs
@@ -2,12 +2,18 @@ use libraw_sys::*;
 use std::ffi::{CString, CStr};
 use std::slice;
 use libc::c_int;
+use image::{GenericImage, ImageBuffer};
 
 #[derive(Debug)]
 pub struct RawFile {
     filename: CString,
     libraw: *mut libraw_data_t,
 }
+
+/// TODO: document. Should this be usize?
+const DBG_CROP_FACTOR: u32 = 1;
+// can't remember if this is right or not.
+const BITS_PER_PIXEL: usize = 14;
 
 fn open_file(libraw: *mut libraw_data_t, filename: &CString) -> Result<(), & 'static str> {
     with_err_conversion(|| unsafe {
@@ -57,17 +63,66 @@ impl RawFile {
     }
 
     pub fn get_jpeg_thumbnail(&self) -> &[u8] {
-         unsafe {
-            unpack_thumb(self.libraw).unwrap();
-            let thumb = &(*self.libraw).thumbnail;
-            let format_code = &thumb.tformat;
-            match *format_code {
-                LibRaw_thumbnail_formats_LIBRAW_THUMBNAIL_JPEG => (),
-                _ => panic!("Expected JPEG thumbnail format"),
-            }
-            println!("size: {}", thumb.tlength);
+        // this is safe to call multiple times.
+        unpack_thumb(self.libraw).unwrap();
+        let thumb = unsafe {
+            &(*self.libraw).thumbnail
+        };
+
+        let format_code = &thumb.tformat;
+        match *format_code {
+            LibRaw_thumbnail_formats_LIBRAW_THUMBNAIL_JPEG => (),
+            _ => panic!("Expected JPEG thumbnail format"),
+        }
+        println!("size: {}", thumb.tlength);
+
+        unsafe {
             slice::from_raw_parts(thumb.thumb as *mut u8, thumb.tlength as usize)
         }
+    }
+
+    pub fn render_raw_preview(&self) -> image::RgbImage {
+        let sizes = unsafe {(*self.libraw).sizes};
+        let img_data = self.load_raw_data();
+        let mapping = unsafe {(*self.libraw).idata.xtrans_abs};
+
+        let img = ImageBuffer::from_fn(
+            sizes.raw_width as u32 / DBG_CROP_FACTOR,
+            sizes.raw_height as u32 / DBG_CROP_FACTOR,
+            |x, y| {
+                let idx = y*(sizes.width as u32) + x;
+                // TODO: this should be a generic call to some kind of demosaic algorithm.
+                let pixel = RawFile::map_x_trans(x, y, sizes.raw_width as u32, sizes.raw_height as u32, img_data, mapping);
+                pixel
+            }
+        );
+        img
+    }
+
+    fn map_x_trans(x: u32, y: u32, width: u32, _height: u32, data: &[u16], mapping: [[i8; 6]; 6]) -> image::Rgb<u8> {
+        let idx = (y*(width as u32) + x) as usize;
+        // TODO: 8 is the target per-channel size here, encode this with generics probably.
+        let val = (data[idx] >> (BITS_PER_PIXEL - 8)) as u8;
+        match mapping[x as usize % 6][y as usize % 6] {
+            0 => image::Rgb([val, 0, 0]), // red
+            1 => image::Rgb([0, val >> 1, 0]), // green
+            2 => image::Rgb([0, 0, val]), // blue
+            _ => panic!("Got unexpected value in xtrans mapping"),
+        }
+    }
+
+    fn load_raw_data(&self) -> (&[u16]) {
+        with_err_conversion(|| unsafe {
+            libraw_unpack(self.libraw)
+        }).unwrap();
+        let sizes = unsafe {(*self.libraw).sizes};
+        // convert to u32 to avoid overflow
+        let num_shorts = sizes.raw_width as usize * sizes.raw_height as usize;
+        // TODO: actually render
+        let img_data = unsafe {
+            slice::from_raw_parts((*self.libraw).rawdata.raw_image, num_shorts)
+        };
+        img_data
     }
 }
 
@@ -76,6 +131,7 @@ impl Drop for RawFile {
         println!("Dropping!");
         unsafe {
             // TODO: unclear if we need libraw_recycle here too.
+            libraw_recycle(self.libraw);
             libraw_close(self.libraw);
         }
     }
