a5488177db283d806ae72cb522ac80da1045667e
Fabian Tamp <fabian.tamp@gmail.com>
2020-01-14 15:08:44 -0500

    Ok, this is as far as I got last time

--- blitz/src/main.rs
+++ blitz/src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate clap;
 
+use image::ImageBuffer;
 use std::fs::File;
 use std::io::Write;
 
@@ -18,13 +19,19 @@ fn main() {
             (author: "Someone E. <someone_else@other.com>")
             (@arg verbose: -v --verbose "Print test information verbosely")
         )
-    ).get_matches();
+    )
+    .get_matches();
 
     let preview_filename = "/tmp/thumb.jpg";
+    let raw_preview_filename = "/tmp/render.jpg";
     let file = libraw::RawFile::open(matches.value_of("INPUT").unwrap()).unwrap();
     println!("Opened file: {:?}", file);
     dump_to_file(preview_filename, file.get_jpeg_thumbnail()).unwrap();
-    open_preview(preview_filename)
+    let preview = render_raw_preview(&file);
+    println!("Saving");
+    preview.save(raw_preview_filename).unwrap();
+    println!("Done saving");
+    open_preview(raw_preview_filename)
 }
 
 fn dump_to_file(filename: &str, data: &[u8]) -> std::io::Result<()> {
@@ -37,8 +44,57 @@ fn dump_to_file(filename: &str, data: &[u8]) -> std::io::Result<()> {
 fn open_preview(filename: &str) {
     use std::process::Command;
 
-    let output = Command::new("open")
+    Command::new("open")
         .arg(filename)
         .spawn()
         .expect("Failed to start");
+}
+
+const DBG_CROP_FACTOR: u32 = 1;
+const BITS_PER_PIXEL: u16 = 14;
+
+fn render_raw_preview(img: &libraw::RawFile) -> image::RgbImage {
+    let sizes = img.img_params();
+    println!("Loading RAW data");
+    let img_data = img.load_raw_data();
+    println!("Done loading; rendering");
+    let mapping = img.xtrans_pixel_mapping();
+
+    let buf = ImageBuffer::from_fn(
+        sizes.raw_width as u32 / DBG_CROP_FACTOR,
+        sizes.raw_height as u32 / DBG_CROP_FACTOR,
+        |x, y| {
+            // TODO: this should be a generic call to some kind of demosaic algorithm.
+            let pixel = map_x_trans(
+                x,
+                y,
+                sizes.raw_width as u32,
+                sizes.raw_height as u32,
+                img_data,
+                mapping,
+            );
+            pixel
+        },
+    );
+    println!("Done rendering");
+    buf
+}
+
+fn map_x_trans(
+    x: u32,
+    y: u32,
+    width: u32,
+    _height: u32,
+    data: &[u16],
+    mapping: &[[i8; 6]; 6],
+) -> image::Rgb<u8> {
+    let idx = (y * (width as u32) + x) as usize;
+    // TODO: 8 is the target per-channel size here, encode this with generics probably.
+    let val = (data[idx] >> (BITS_PER_PIXEL - 8)) as u8;
+    match mapping[x as usize % 6][y as usize % 6] {
+        0 => image::Rgb([val, 0, 0]),      // red
+        1 => image::Rgb([0, val >> 1, 0]), // green
+        2 => image::Rgb([0, 0, val]),      // blue
+        _ => panic!("Got unexpected value in xtrans mapping"),
+    }
 }
--- libraw/src/lib.rs
+++ libraw/src/lib.rs
@@ -9,15 +9,18 @@ mod tests {
 #[macro_use]
 extern crate quick_error;
 
-use std::path::Path;
 use libraw_sys::*;
-use std::ffi::CString;
+use std::ffi::{CStr, CString};
+use std::os::raw::c_int;
+use std::path::Path;
 use std::slice;
 
 quick_error! {
     #[derive(Debug)]
     pub enum RawError {
         NotFound {}
+        GenericError(message: &'static str) {}
+        LibraryError {}
     }
 }
 
@@ -25,17 +28,20 @@ type Result<T> = std::result::Result<T, RawError>;
 
 #[derive(Debug)]
 pub struct RawFile {
-    libraw: * mut libraw_data_t,
+    libraw: *mut libraw_data_t,
+    // TODO: figure out how getters work
     img_params: ImgParams,
 }
 
 #[derive(Debug)]
-struct ImgParams {
-    rheight: u32,
-    rwidth: u32,
-    iheight: u32,
-    iwidth: u32,
+pub struct ImgParams {
+    pub raw_height: u32,
+    pub raw_width: u32,
+    pub image_height: u32,
+    pub image_width: u32,
 }
+
+type XTransPixelMap = [[i8; 6]; 6];
 
 impl Drop for RawFile {
     fn drop(&mut self) {
@@ -45,19 +51,35 @@ impl Drop for RawFile {
     }
 }
 
+fn with_err_conversion<F>(f: F) -> Result<()>
+where
+    F: Fn() -> c_int,
+{
+    unsafe {
+        match f() {
+            0 => Ok(()),
+            code => {
+                let err = CStr::from_ptr(libraw_strerror(code));
+                Err(RawError::GenericError(err.to_str().unwrap()))
+            }
+        }
+    }
+}
 
+// TODO: the error-handling story in general is pretty bad - libraw has a bunch
+// of errors that mean "I gave up and your object is no longer valid" and we
+// don't handle those super well. See https://www.libraw.org/docs/API-datastruct.html#LibRaw_errors
+// TODO: some of these methods probably need to receive &mut self.
 impl RawFile {
     pub fn open<P: AsRef<Path>>(path: P) -> Result<RawFile> {
-        // TODO: handle null (represents an error)
-        let libraw = unsafe {
-            libraw_init(0)
-        };
+        let libraw = unsafe { libraw_init(0) };
+        if libraw.is_null() {
+            return Err(RawError::LibraryError);
+        }
         // TODO: this is only legit if Path is valid unicode.
         let img_path = path.as_ref().to_str().unwrap();
         let cstr = CString::new(img_path).unwrap();
-        let code = unsafe {
-            libraw_open_file(libraw, cstr.as_ptr())
-        };
+        with_err_conversion(|| unsafe { libraw_open_file(libraw, cstr.as_ptr()) })?;
 
         let (rheight, rwidth, iheight, iwidth) = unsafe {
             (
@@ -68,38 +90,48 @@ impl RawFile {
             )
         };
 
-        // TODO: check code
         Ok(RawFile {
             libraw,
-            // TODO: maybe this can be loaded directly from the thingy?
+            // TODO: maybe this can be loaded directly from the pointer
             img_params: ImgParams {
-                iheight,
-                iwidth,
-                rheight,
-                rwidth,
+                image_height: iheight,
+                image_width: iwidth,
+                raw_height: rheight,
+                raw_width: rwidth,
             },
         })
     }
 
+    pub fn img_params(&self) -> &ImgParams {
+        &self.img_params
+    }
+
+    pub fn xtrans_pixel_mapping(&self) -> &XTransPixelMap {
+        unsafe { &(*self.libraw).idata.xtrans_abs }
+    }
+
     pub fn get_jpeg_thumbnail(&self) -> &[u8] {
         // this is safe to call multiple times.
-        let error = unsafe {
-            libraw_unpack_thumb(self.libraw)
-        };
-        // TODO check code
-        let thumb = unsafe {
-            (*self.libraw).thumbnail
-        };
+        with_err_conversion(|| unsafe { libraw_unpack_thumb(self.libraw) }).unwrap();
+        let thumb = unsafe { (*self.libraw).thumbnail };
 
         let format_code = &thumb.tformat;
+
+        #[allow(non_upper_case_globals)]
         match *format_code {
             LibRaw_thumbnail_formats_LIBRAW_THUMBNAIL_JPEG => (),
             _ => panic!("Expected JPEG thumbnail format"),
         }
         println!("size: {}", thumb.tlength);
 
-        unsafe {
-            slice::from_raw_parts(thumb.thumb as *const u8, thumb.tlength as usize)
-        }
+        unsafe { slice::from_raw_parts(thumb.thumb as *const u8, thumb.tlength as usize) }
+    }
+
+    pub fn load_raw_data(&self) -> &[u16] {
+        with_err_conversion(|| unsafe { libraw_unpack(self.libraw) }).unwrap();
+        let sizes = unsafe { (*self.libraw).sizes };
+        // convert to u32 to avoid overflow
+        let num_shorts = sizes.raw_width as usize * sizes.raw_height as usize;
+        unsafe { slice::from_raw_parts((*self.libraw).rawdata.raw_image, num_shorts) }
     }
 }
